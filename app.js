50b8b42c3eea33db8e92e0e210f4468a75e63cc3
dhilippan <dhilippan1997@gmail.com>
2022-01-13 15:51:20 +0530

    first commit

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 let player1wins = 0;
 let player2wins = 0;
 
-function loadDoc() {
-  if (player2wins < 3 && player1wins < 3) {
+function playgame() {
+  if (player1wins < 3 && player2wins < 3) {
     let player1H = 100;
     let player2H = 100;
     while (player1H > 0 && player2H > 0) {
@@ -13,11 +13,11 @@ function loadDoc() {
     }
     if (player1H > 0) {
       player1wins += 1;
-      console.log("player1:" + player1wins)
+      // console.log("player1:" + player1wins)
       document.getElementById("p1").innerHTML = player1wins;
     } else {
       player2wins += 1;
-      console.log("player2:" + player2wins)
+      // console.log("player2:" + player2wins)
       document.getElementById("p2").innerHTML = player2wins;
     }
   }
@@ -26,6 +26,6 @@ function loadDoc() {
     document.getElementById("final").innerHTML = "Player1 won the match!";
   }
   if (player2wins >= 3) {
-    document.getElementById("final").innerHTML = "Player1 won the match!";
+    document.getElementById("final").innerHTML = "Player2 won the match!";
   }
 }
